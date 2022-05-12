67ef30b41895dcad664e60c14a16e4f8bc1d13ce
plokhykh <mplokhykh@gmail.com>
2022-05-12 23:20:13 +0300

    lesson1

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IPost} from "../models/IPost";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getPosts(): Observable<IPost[]> {
+    return this.http.get<IPost[]>(this.url);
+  }
+}
